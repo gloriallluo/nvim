64a693db00f5b5da1d43281558096315a15c2373
Jingjia Luo <jingjia_luo@163.com>
2025-09-18 09:02:56 +0000

    update

--- init.lua
+++ init.lua
@@ -10,6 +10,46 @@ vim.g.loaded_netrwPlugin = 1
 
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
+
+local cmp = require("cmp")
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-h>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-l>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' }
+    }
+})
+
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { 
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { 'Man', '!' }
+            }
+        }
+    })
+})
+
 require("bufferline").setup({
     options = {
         offsets = {
@@ -49,7 +89,9 @@ require("nvim-tree").setup({
 
 require("telescope").setup({})
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("lspconfig").clangd.setup({
+    capabilities = capabilities,
     cmd = {'clangd', '--background-index', '--clang-tidy', '--log=verbose'},
     init_options = {
         fallbackFlags = { '-std=c++17' },
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -23,15 +23,15 @@ vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
 -- Bufferline
-vim.keymap.set('n', '<S-[>', ':BufferLineCyclePrev<CR>', opts)
-vim.keymap.set('n', '<S-]>', ':BufferLineCycleNext<CR>', opts)
+vim.keymap.set('n', '<C-[>', ':BufferLineCyclePrev<CR>', opts)
+vim.keymap.set('n', '<C-]>', ':BufferLineCycleNext<CR>', opts)
 
 -- Telescope
 local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, opts)
-vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, opts)
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, opts)
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, opts)
+vim.keymap.set('n', '<leader>f', telescope_builtin.find_files, opts)
+vim.keymap.set('n', '<leader>g', telescope_builtin.live_grep, opts)
+vim.keymap.set('n', '<leader>b', telescope_builtin.buffers, opts)
+vim.keymap.set('n', '<leader>h', telescope_builtin.help_tags, opts)
 
 -----------------
 -- Visual mode --
